7ade3d3ac6a8985ac7c940aa69a4caafb3b8d0e2
stltanaka <sheatltanaka@gmail.com>
2024-10-23 19:38:16 -0700

    Update script.js

--- finn/script.js
+++ finn/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", generateBingoCard);
 document.getElementById('generate-button').addEventListener('click', generateBingoCard);
+document.addEventListener("DOMContentLoaded", getUserstate);
 
 function generateBingoCard() {
     const input = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25"
@@ -15,6 +16,8 @@ function generateBingoCard() {
         cell.textContent = word;
         cell.addEventListener('click', () => cell.classList.toggle('checked'));
         bingoBoard.appendChild(cell);
+    
+    setUserstate
     });
 }
 
