91692a4dd2708cf3f4bb9ecb54cb5a5eaf53cb71
stltanaka <sheatltanaka@gmail.com>
2024-10-24 01:02:42 -0700

    Update script.js

--- finn/script.js
+++ finn/script.js
@@ -1,13 +1,32 @@
 document.addEventListener("DOMContentLoaded", generateBingoCard);
 document.getElementById('generate-button').addEventListener('click', generateBingoCard);
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzXUkX5xmJHLwuPUaiQpgOzuQ9oSTkaTGdYuCQRlz08YzLFAo6UZsN0Ii3S_oQbG16qkg/exec';
 
 function generateBingoCard() {
+    document.getElementById('statusMessage').style.visibility = 'visible';
     const input = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25"
     const words = input.split(',').map(word => word.trim()).filter(word => word);
 
     const shuffledWords = shuffleArray(words).slice(0, 25);
     const bingoBoard = document.getElementById('bingo-board');
     bingoBoard.innerHTML = '';
+    fetch(scriptURL)
+        .then(response => response.json())
+        .then(words => {
+            const shuffledWords = shuffleArray(words).slice(0, 25);
+                    
+            shuffledWords.forEach((word) => {
+                const cell = document.createElement('div');
+                cell.className = 'bingo-cell';
+                cell.textContent = word;
+                cell.addEventListener('click', () => cell.classList.toggle('checked'));
+                bingoBoard.appendChild(cell);
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error))
+		.finally(() => {
+                document.getElementById('statusMessage').style.visibility = 'hidden';
+        });
     
     shuffledWords.forEach((word, index) => {
         const cell = document.createElement('div');
