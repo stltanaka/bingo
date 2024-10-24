8366c983c936d6a746fa6cd812e5ec70cdcaf7c0
stltanaka <sheatltanaka@gmail.com>
2024-10-23 17:51:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('generate-button').addEventListener('click', generateBin
 document.getElementById('reset-button').addEventListener('click', resetGame);
 
 function generateBingoCard() {
-    const input = 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
+    const input = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25"
     const words = input.split(',').map(word => word.trim()).filter(word => word);
     }
 
